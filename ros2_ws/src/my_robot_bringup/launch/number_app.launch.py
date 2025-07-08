4f9d30f8e0407b3aa9879f6237ab38e7b83bade9
Mir Misbah Ali <mirmisbahali@hotmail.com>
2025-07-08 16:53:03 +1000

    import parameters from yaml file

--- ros2_ws/src/my_robot_bringup/launch/number_app.launch.py
+++ ros2_ws/src/my_robot_bringup/launch/number_app.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -9,7 +10,11 @@ def generate_launch_description():
         package="my_py_pkg",
         executable="number_publisher",
         name="my_number_publisher",
-        remappings=[("/number", "/my_number")]
+        remappings=[("/number", "/my_number")],
+        parameters=[
+            {"number": 12},
+            {"timer_period": 1.3}
+        ]
     )
 
     number_counter = Node(
